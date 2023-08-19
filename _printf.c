5205584a85a1029695c42855c27845164cd84759
Mutahe <elizagatura@gmail.com>
2023-08-19 15:30:04 -0700

    adding file Co-authored-by: Bobbyzain <abhameed419@gmail.com>

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf -  produces output according to a format
+ * @format: string containing directives
+ * return: always integer
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i;
+}
