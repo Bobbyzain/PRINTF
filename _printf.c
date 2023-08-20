749010dc3e8fe582dab6e7d508b7b376f35ddcf6
Mutahe <elizagatura@gmail.com>
2023-08-19 17:01:23 -0700

    adding file Co-authored-by: Bobbyzain <abhameed419@gmail.com>

--- _printf.c
+++ _printf.c
@@ -9,5 +9,32 @@
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i;
+	int written = 0;
+	const char *c;
+
+
+	va_start(ap, format);
+	for (; *c != '\0'; c++)
+	{
+		if (*c == '%')
+		{
+			c++;	/* to move beyond the % */
+			if (*c == '\0')	/* end of string (unexpected) */
+				break;
+			if (*c == '%')
+			{
+				write(1, "%", 1);	/* handle '%%' case */
+				written++;
+			}
+			/* else */
+				/* written == _printf_formats1(c, written, ap); * to handle format specifier */
+		}
+		else
+		{
+			write(1, c, 1);	/* this is a regular character */
+			written++;
+		}
+	}
+	va_end(ap);
+	return (written);
 }
