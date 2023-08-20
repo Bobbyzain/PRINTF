a13c1ccbc14138131fe6f622b0751137de92801b
Mutahe <elizagatura@gmail.com>
2023-08-20 15:58:05 -0700

    adding file Co-authored-by: Bobbyzain <abhameed419@gmail.com>

---
+++ address_imp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * address_print - print the address in hex
+ *
+ * @written: argument counter
+ * @ap: argument parameter
+ *
+ * Return: written as integer
+ */
+int address_print(int written, va_list ap)
+{
+	void *ptr;
+	int len = 0;
+
+	ptr = va_arg(ap, void*);
+	if (ptr == NULL)
+	{
+		len += write(1, "(nil)", 5);
+	}
+	else
+	{
+		len += write(1, "0x", 2);
+	}
+	len += pointer_writer(ptr);
+	written += len;
+	return (written);
+}
