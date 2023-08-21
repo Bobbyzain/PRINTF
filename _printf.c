499ece6d299eec0ed593244fa46ea28d79ab373f
Bobbyzain <abhameed419@gmail.com>
2023-08-21 15:59:02 -0700

    Co-authored-by: Mutahe <elizagatura@gmail.com>

--- _printf.c
+++ _printf.c
@@ -26,8 +26,8 @@ int _printf(const char *format, ...)
 				write(1, "%", 1);	/* handle '%%' case */
 				written++;
 			}
-			/* else */
-				/* written == _printf_formats1(c, written, ap); * to handle format specifier */
+			else
+				written == _printf_formats1(c, written, ap); /* to handle format specifier */
 		}
 		else
 		{
@@ -47,5 +47,39 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(ap);
+	return (written);
+}
+/**
+ * _printf_formats1 - continuation of the printf variadic function
+ * @cc: string containing directives or specifiers
+ * @written: argument parameters tracker
+ * @ap: argument parameters
+ * Return: Int
+ */
+int _printf_formats1(const char *cc, int written, va_list ap)
+{
+	const char *c = cc;
+	char chc;
+
+	if (*c == 'u')
+		written = uns_spec(written, ap);
+	else if (*c == 'c')
+	{
+		chc = va_arg(ap, int); /* Handle for character format*/
+		written += write(1, &chc, 1);
+	}
+	else if (*c == 'p')
+		written = address_print(written, ap); /*Handle printing of addresses */
+	else if (*c == '+')
+	{
+		c++;
+		if (*c == 'd' || *c == 'i' || *c == 'f')
+			written = signed_int_spec(written, ap); /* Handle signs in stdout */
+		else
+		{
+			c--;
+			write(1, c, 1);
+		}
+	}
 	return (written);
 }
